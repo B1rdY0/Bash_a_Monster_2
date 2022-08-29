61798dfc198d4aa9f7d5161c308b6212bcac75b9
Aaryan hegde <hegde.aaryan@gmail.com>
2022-08-29 13:21:30 +0200

    jwepigwoern

---
+++ Bash_a_monster/Bash_a_monster/Player.cs
@@ -0,0 +1,18 @@
+using System;
+
+
+
+public class Player
+{
+   public int HP = 100;
+   public int DMG = 50;
+
+}
+
+public class Boss
+{
+    public int Life = 1000;
+
+    public int DMGB = 1;
+
+}
---
+++ Bash_a_monster/Bash_a_monster/Program.cs
@@ -0,0 +1,40 @@
+﻿string svar;
+Player player = new Player();
+Boss Boss = new Boss();
+
+
+while (player.HP > 0 && Boss.Life > 0)
+{
+    Console.WriteLine("punch or take hit");
+    svar = Console.ReadLine();
+    
+
+    if(svar == "punch")
+    {
+       Boss.Life = Boss.Life - player.DMG; 
+
+       Console.WriteLine($"The boss has {Boss.Life} HP");
+    }
+
+    if (svar == "take hit")
+    {
+      player.HP = player.HP - Boss.DMGB;  
+
+      Console.WriteLine($"u have {player.HP} HP");
+    }
+    
+}
+
+
+if(player.HP < 0){
+    System.Console.WriteLine("Game over");
+}
+
+if(Boss.Life < 0){
+    Console.WriteLine("U win");
+}
+
+
+
+
+
